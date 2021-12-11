3082f8b7cc114c3385d1c42a1a460da7ec557d52
Slobodan.Vukovojac <Slobodan.vukovojac@computerrock.com>
2021-12-11 11:37:17 +0100

    [misc] creating input component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Input from './ui-components/inputs/Input';
 import './App.scss';
 
 function App() {
+  const [message, setMessage] = useState('');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Input placeholder="Message" onChange={data => setMessage(data)} value={message} name="message" />
     </div>
   );
 }
